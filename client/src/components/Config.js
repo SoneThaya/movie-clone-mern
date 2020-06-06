32d602ca600a6ca072178775bd3b3e7ebb66f4cf
Sone <sthaya@gmail.com>
2020-06-06 14:27:25 -0700

    started movie clone mern project

---
+++ client/src/components/Config.js
@@ -0,0 +1,8 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+
+export const API_KEY = 'ff1943536fc85cf8c8726a9decdd8ff2';
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_URL = 'http://image.tmdb.org/t/p/';
